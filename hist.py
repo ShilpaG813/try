28eb8cd9351c0477745ff4ee46aeb906d4e96b64
Shilpa.G <shilpagshilpag813@gmail.com>
2023-08-09 10:46:19 +0530

    added

--- hist.py
+++ hist.py
@@ -5,4 +5,5 @@ a=t[t['Survived']==1]['Age']
 d=t[t['Survived']==0]['Age']
 plt.hist(a,label='Survived',color='b',alpha=1)
 plt.hist(d,label='Not survived',color='g',alpha=0.6)
+plt.xlabel('Age')
 plt.show()
